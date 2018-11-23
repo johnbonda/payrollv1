0e92f843c92acfb1d123db4ef3d75d833d946ca6
johnbonda <john@belfricsbt.com>
2018-11-23 16:17:28 +0530

    Added tokens

--- interface/authController.js
+++ interface/authController.js
@@ -3,7 +3,7 @@ var jwt = require('jsonwebtoken');
 
 module.exports.getJwt = function(email){
     // create a token
-    var token = jwt.sign({ id: email, timestamp: new Date().getTime() }, config.secret, {
+    var token = jwt.sign({ id: email, timestamp: new Date().getTime() }, config.tokenKey, {
         expiresIn: 86400 // expires in 24 hours
       });
     return token;
